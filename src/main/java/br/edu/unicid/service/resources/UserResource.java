beb4731c0702ed6981fd861fc05b5f7878e82986
Ricardo Fernando <ricardo347@msn.com>
2020-01-27 19:40:36 -0300

    User entity and resource

---
+++ src/main/java/br/edu/unicid/service/resources/UserResource.java
@@ -0,0 +1,19 @@
+package br.edu.unicid.service.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.unicid.service.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Mary", "wer@sadf","23234","pass");
+		return ResponseEntity.ok().body(u);
+	} 
+}
