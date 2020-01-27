0ad30840b66f4f266dbae66071188d6df7490c20
Ricardo Fernando <ricardo347@msn.com>
2020-01-27 18:57:32 -0300

    Project Created

---
+++ src/main/java/br/edu/unicid/service/RmServiceNfeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unicid.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmServiceNfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmServiceNfeApplication.class, args);
+	}
+
+}
