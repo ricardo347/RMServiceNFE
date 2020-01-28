05a7832f261229cf225151f37e04fd482f0f24f8
Ricardo Fernando <ricardo347@msn.com>
2020-01-28 11:28:30 -0300

    h2 setup

--- src/main/java/br/edu/unicid/service/entities/User.java
+++ src/main/java/br/edu/unicid/service/entities/User.java
@@ -2,10 +2,17 @@ package br.edu.unicid.service.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -17,7 +24,8 @@ public class User implements Serializable{
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
+		
+		
 		this.id = id;
 		this.name = name;
 		this.email = email;
