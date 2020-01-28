e27e57974170e84aebc95d19c25f2ac2d97347c0
Ricardo Fernando <ricardo347@msn.com>
2020-01-28 15:04:42 -0300

    Service layer, component registration

--- src/main/java/br/edu/unicid/service/resources/UserResource.java
+++ src/main/java/br/edu/unicid/service/resources/UserResource.java
@@ -1,19 +1,37 @@
 package br.edu.unicid.service.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.unicid.service.entities.User;
+import br.edu.unicid.service.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	//when using spring dependency injection, it must register de component
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Mary", "wer@sadf","23234","pass");
-		return ResponseEntity.ok().body(u);
-	} 
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	//value means, we're passing parameter id to GET
+	//@PathVariable must do be add to method parameter
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
