bf2ad30efab7ef1b328d55dcbe2001e329f26fa9
Ricardo Fernando <ricardo347@msn.com>
2020-01-29 19:37:31 -0300

    Subtotal & Total methods

--- src/main/java/br/edu/unicid/service/entities/Order.java
+++ src/main/java/br/edu/unicid/service/entities/Order.java
@@ -105,6 +105,14 @@ public class Order implements Serializable{
 		if (orderStatus != null)
 			this.orderStatus = orderStatus.getCode();
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
